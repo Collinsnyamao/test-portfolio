44efe7e2b0c5299f68ca28e0ed0b555889079cc3
Oldnickel <nyamaocollins@gmail.com>
2024-09-02 15:03:11 +0300

    test change trigger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-900 text-white text-4xl font-bold">Cnyamao Portfolio</div>
+    <div className="w-full h-screen flex justify-center items-center bg-gray-900 text-white text-4xl font-bold">C. nyamao Portfolio</div>
   );
 }
 
