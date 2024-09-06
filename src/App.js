7df8a6e074c45d2b94afe7cee4dcf1dc22c58bd3
Oldnickel <nyamaocollins@gmail.com>
2024-09-06 21:05:33 +0300

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div
-      className="w-full h-screen flex justify-center items-center bg-gray-900 text-white text-4xl font-bold">C. nyamao Portfolio</div>
+      className="w-full h-screen flex justify-center items-center bg-gray-900 text-white text-4xl font-bold">C. nyamao Portfolio.</div>
   );
 }
 
